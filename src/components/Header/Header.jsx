9fc1404fd355c33a7b74f9cb37d7eb3ce0e5dac4
Garri3105 <nikola.novisad2@gmail.com>
2026-02-03 18:29:55 +0100

    potencijalni fix 500 (popravljen header za user-a)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,13 @@
+import { useAuth } from "../../auth/AuthContext"
 import "./Header.css"
 
-function Header() {
-    return (
-        <header className="header">
-            <h2>CS2 Wheel</h2>
-            <div>
-                <span>{user?.username}</span>
-                <button onClick={logout}>Logout</button>
-            </div>
-        </header>
-    )
-}
+export default function Header() {
+  const { user, logout } = useAuth()
 
-export default Header
+  return (
+    <header>
+      <div>{user ? `Ulogovan: ${user.username}` : "Nisi ulogovan"}</div>
+      {user && <button onClick={logout}>Logout</button>}
+    </header>
+  )
+}
